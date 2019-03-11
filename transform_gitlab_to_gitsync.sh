b113f55d54d71de4d6b2ecde8799996ae5859dea
Maximilian Mayer <mayer.maximilian@googlemail.com>
2019-03-11 15:00:40 +0100

    update transform script to use var instead of hardcoded filename

--- transform_gitlab_to_gitsync.sh
+++ transform_gitlab_to_gitsync.sh
@@ -3,7 +3,7 @@
 input_file=$1
 if [ -z ./.gitsync ];then
     echo -e "---\nrepos:" > .gitsync
-    jq '.[]| "  " +.name +":", "    url: "+ (.ssh_url_to_repo)' gitsync.json | sed -e 's/"//g'>> .gitsync
+    jq '.[]| "  " +.name +":", "    url: "+ (.ssh_url_to_repo)' ${input_file} | sed -e 's/"//g'>> .gitsync
 
 else
     echo "Error: .gitsync file is not empty!"
